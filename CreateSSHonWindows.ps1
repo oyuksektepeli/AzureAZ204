a11c80abd3eb78310025d89122833bab9590721f
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-13 14:35:01 +0300

    create azure vm with azure cli

---
+++ CreateSSHonWindows.ps1
@@ -0,0 +1,29 @@
+# Install the OpenSSHUtils module to the server. This will be valuable when deploying user keys.
+Install-Module -Force OpenSSHUtils -Scope AllUsers
+
+# Start the ssh-agent service to preserve the server keys
+Start-Service ssh-agent
+
+# Now start the sshd service
+Start-Service sshd
+
+cd ~\.ssh\
+ssh-keygen
+
+# Make sure you're running as an Administrator
+Start-Service ssh-agent
+
+# This should return a status of Running
+Get-Service ssh-agent
+
+# Now load your key files into ssh-agent
+ssh-add ~\.ssh\id_ed25519
+
+# Make sure that the .ssh directory exists in your server's home folder
+ssh user1@domain1@contoso.com mkdir C:\users\user1\.ssh\
+
+# Use scp to copy the public key file generated previously to authorized_keys on your server
+scp C:\Users\user1\.ssh\id_ed25519.pub user1@domain1@contoso.com:C:\Users\user1\.ssh\authorized_keys
+
+# Appropriately ACL the authorized_keys file on your server  
+ssh --% user1@domain1@contoso.com powershell -c $ConfirmPreference = 'None'; Repair-AuthorizedKeyPermission C:\Users\user1\.ssh\authorized_keys
---
+++ ResourceLinks.md
@@ -0,0 +1,31 @@
+# Azure Resources
+
+**Azure CLI**\
+https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
+
+**Azure Powershell**\
+https://docs.microsoft.com/en-us/powershell/azure/?view=azps-3.6.1
+
+**Visual Studio Community**\
+https://visualstudio.microsoft.com/vs/community/
+
+**Visual Studio Code**\
+https://code.visualstudio.com/
+
+**Azure Resources Naming Conventions**\
+https://docs.microsoft.com/en-us/azure/architecture/best-practices/naming-conventions
+
+**Azure Services**\
+https://nnmer.github.io/azure-services-map/dist/
+
+**Azure Guick Start Templates Github**\
+https://github.com/Azure/azure-quickstart-templates
+
+**Azure Resource Manager**\
+https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-overview
+
+**Azure Speed**\
+http://www.azurespeed.com/
+
+**Azure Virtual Machine Series**\
+https://azure.microsoft.com/en-us/pricing/details/virtual-machines/series/
