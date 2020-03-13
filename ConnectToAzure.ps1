c9c72824ed18c8fb9116a3ad4bfc5029bd2e20c3
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-13 15:51:56 +0300

    Connect Azure commands

---
+++ ConnectToAzure.ps1
@@ -0,0 +1,26 @@
+#Connect Azure with Powershell
+Install-Module -Name Az -AllowClobber -Scope CurrentUser
+
+#Connect and Authenticate
+Connect-AzAccount
+
+#Get Current Connected Subscription/Context
+Get-AzureRmContext
+
+#Get All Azure Subscriptions
+Get-AzSubscription | more
+
+#Select Azure Subscription
+Select-AzContext
+
+
+#Connect Azure with Azure CLI
+
+#Connect and Authenticate
+Az login
+
+#Get All Azure Subscriptions
+az account list
+
+#Select Azure Subscription
+az account set --subscription "subscription-name"
