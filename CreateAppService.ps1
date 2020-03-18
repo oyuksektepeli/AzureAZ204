7aa222b194fad847386fa6c08a91c5bfad3fca21
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-18 15:04:39 +0300

    create app service with powershell

---
+++ CreateAppService.ps1
@@ -0,0 +1,17 @@
+# Replace the following URL with a public GitHub repo URL
+$gitrepo="https://github.com/Azure-Samples/app-service-web-dotnet-getstarted.git"
+$webappname="mywebapp$(Get-Random)"
+$location="West Europe"
+# Create a resource group.
+New-AzureRmResourceGroup -Name myResourceGroup -Location $location
+# Create an App Service plan in Free tier.
+New-AzureRmAppServicePlan -Name $webappname -Location $location -ResourceGroupName myResourceGroup -Tier Free
+# Create a web app.
+New-AzureRmWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName myResourceGroup
+# Configure GitHub deployment from your GitHub repo and deploy once.
+$PropertiesObject = @{
+ repoUrl = "$gitrepo";
+ branch = "master";
+ isManualIntegration = "true";
+}
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName myResourceGroup -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webappname/web -ApiVersion 2015-08-01 -Force
