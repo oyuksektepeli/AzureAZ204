cb9fe8f387e510c2c3c5849cac4dca2e37242e72
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-26 22:59:39 +0300

    deploy logic app

--- DeployLogicApp.ps1
+++ DeployLogicApp.ps1
@@ -1,5 +1,6 @@
 #deploy with Powershell
-New-AzResourceGroupDeployment -ResourceGroupName <Azure-resource-group-name> -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-logic-app-create/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName my-resource-group -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-logic-app-create/azuredeploy.json
 
 #Deploy with AzureCLI
-az group deployment create -g <Azure-resource-group-name> --template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-logic-app-create/azuredeploy.json
+az group deployment create -g my-resource-group --template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-logic-app-create/azuredeploy.json
+
